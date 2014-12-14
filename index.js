3449b7ad6daad6385aed7aadcd82bf1432de09a3
fritzvd <f.vandeventer@gmail.com>
2014-12-14 21:08:05 +0100

    meah

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var THREE = require('three');
 
-var scene, camera, renderer;
+var scene, camera, renderer, pointLight;
 
 var geometry, material, mesh;
 
@@ -12,6 +12,12 @@ function init () {
 
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+  pointLight = new THREE.PointLight(0xFFFFFF);
+
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
+  scene.add(pointLight);
 
   geometry = new THREE.BoxGeometry(200, 200, 200);
   material = new THREE.MeshBasicMaterial({color: 0xff00ff, wireframe: true});
@@ -32,10 +38,18 @@ function animate() {
 }
 
 function handleInput (e) {
-  if (e.key === "Up") {
-    console.log(camera);
+  switch (e.key) {
+    case "Up":
+    camera.position.y += 1;
+    case "Down":
+    camera.position.y -= 1;
+    case "Left":
+    camera.position.x -= 1;
+    case "Right":
+    camera.position.x += 1;
   }
 
+    e.stopPropagation();
 };
 
 window.addEventListener('keydown', handleInput); 
